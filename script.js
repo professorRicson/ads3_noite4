18d7d119fa8f1ea711527a49ccaca0e8dde81f12
Thalisson Freire <tjsf1@discente.ifpe.edu.br>
2024-07-20 12:41:36 -0300

    Tela de contatos enviando email sem css

---
+++ script.js
@@ -0,0 +1,27 @@
+const res = require("express/lib/response");
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        telefone: document.getElementById("Telefone").value,
+        cidade: document.getElementById("Cidade").value,
+        message: document.getElementById("Mensagem").value
+    }
+
+    const serviceID = "service_plm1f8k";
+    const templateID = "template_e2hvv2k";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("Telefone").value = "";
+                document.getElementById("Cidade").value = "";
+                document.getElementById("Mensagem").value = "";
+                console.log(res);
+                alert("your message sent sucessfully");
+            })
+            .catch((err) => console.log(err));
+}
